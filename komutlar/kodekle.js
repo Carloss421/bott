84bfd30954ecef694b29edf602c6c87db6800a93
Glitch (kod-sunucuv2) <none>
2022-10-09 06:44:53 +0000

    📺🍈 Checkpoint ./komutlar/kodekle.js:40432189/287 ./komutlar/deneme.js:40432189/5758 ./bot.js:40432189/2

--- komutlar/kodekle.js
+++ komutlar/kodekle.js
@@ -21,9 +21,8 @@ if(!args[0]) return message.channel.send(karaliste)
 .setColor("RED")
 .setDescription(`**${message.guild.name} SUNUCUSU KOD PAYLAŞIM KANALI! JavaScript KATEGORİSİ!**\n
             **${message.author} Tarafından \`\`${kanal}\`\` İsimli Kod Eklendi! ${message.author} Teşekkür Ederiz! :heart:\n\n
-            ${client.emojis.get("EMOJİ ID")} İşte Kod;**
+            ${client.emojis.get("EMOJİ ID")} İşte Kod;**`)))
 
-            ${code}`)))
             message.reply(`${client.emojis.get("EMOJİ ID")} **Kodunuz Başarıyla JavaScript Kategorisine Eklendi!\n Eklenen Kanalın İsmi:** \`\`${kanal}\`\` `).then(n => n.delete(5000));
             } else {
             return message.reply(`<#${ayarlar.ID.jskatagoriID}> adında bir kategori yok!`)
@@ -40,10 +39,7 @@ if(!args[0]) return message.channel.send(karaliste)
             if (message.guild.channels.find(a => a.id === ayarlar.ID.js2katagoriID)) {
             message.guild.createChannel(kanal, {type: "text", parent: message.guild.channels.find(a => a.id === ayarlar.ID.js2katagoriID)}).then(c => c.send(new Discord.RichEmbed()
 .setColor("RED")
-.setDescription(
-
-
-            `**${message.guild.name} SUNUCUSU KOD PAYLAŞIM KANALI! JavaScript+ KATEGORİSİ!**\n
+.setDescription(`**${message.guild.name} SUNUCUSU KOD PAYLAŞIM KANALI! JavaScript+ KATEGORİSİ!**\n
             **${message.author} Tarafından \`\`${kanal}\`\` İsimli Kod Eklendi! ${message.author} Teşekkür Ederiz! :heart:\n\n
             ${client.emojis.get("EMOJİ ID")} İşte Kod;**
 
