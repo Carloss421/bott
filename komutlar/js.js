c3d067d6ded0a8214c3cc4049e4a250a0a036a81
Glitch (kod-sunucuv2) <none>
2022-10-09 05:46:58 +0000

    🎿🍀 Checkpoint ./komutlar/js.js:40432189/236 ./ayarlar.json:40432189/90

--- komutlar/js.js
+++ komutlar/js.js
@@ -8,22 +8,22 @@ exports.run = function(client, message) {
 
 /////////////////////Config
 
- var logkanali = ayarlar.logkanali; 
- var rolismi = ayarlar.rolismi; 
-var logkanalid = ayarlar.logkanalid;   
+ var logkanali = ayarlar.log.jslogkanali; 
+ var rolismi = ayarlar.rol.jsrolismi; 
+var logkanalid = ayarlar.log.jslogkanalid;   
   var guildid = ayarlar.sunucuID 
 
 ///////////////// Ana Kod
 message.channel.bulkDelete(1)
-  var role = message.guild.roles.find(i => i.id === ayarlar.js); 
-  if (message.member.roles.has(role.id)) return message.channel.send(`❌ Maalesef, <@&${ayarlar.js}> Role Sahipsin`).then(n => n.delete(5000));
-  if(!role) return message.channel.send(`<@&${ayarlar.js}>`, "Adında Bir Rol Bulamıyorum Lütfen <@"+ayarlar.sahip+"> 'e ulaş" )
-  if(logkanali == "false") {
+  var role = message.guild.roles.find(i => i.id === ayarlar.rol.js); 
+  if (message.member.roles.has(role.id)) return message.channel.send("❌ Maalesef, **"+rolismi+"** Role Sahipsin").then(n => n.delete(5000));
+  if(!role) return message.channel.send(`**<@&${rolismi}>**`, "Adında Bir Rol Bulamıyorum Lütfen <@"+ayarlar.sahip+"> 'e ulaş" )
+  if(logkanali == "true") {
 message.member.addRole(role);
-message.channel.send(`✅  Başarıyla <@&${ayarlar.js}> Rolün verildi!`).then(n => n.delete(5000));
-client.channels.get(logkanalid).send("✅  | " + message.author +" Adlı Kullanıcı Başarılı Bir Şekilde **<@"+rolismi+">** Adlı Rolü Aldı!")
+message.channel.send(`✅  Başarıyla **${rolismi}** Rolün verildi!`).then(n => n.delete(5000));
+client.channels.get(logkanalid).send("✅  | " + message.author +" Adlı Kullanıcı Başarılı Bir Şekilde **"+rolismi+"** Adlı Rolü Aldı!")
 }else {
-message.channel.send('✅  Başarıyla <@&${ayarlar.js}> Rolün verildi!').then(n => n.delete(5000));
+message.channel.send("✅  Başarıyla **"+rolismi+"** Rolün verildi!").then(n => n.delete(5000));
 message.member.addRole(role);
 }
   
